fa3d906719f7a7bf60be617f53a7cf04db83a46f
Adeola Fadare <adeolafadare8@gmail.com>
2026-04-27 11:10:08 +0100

    fix(profile): Format name on create profile

--- src/profile/dto/create-profile.dto.ts
+++ src/profile/dto/create-profile.dto.ts
@@ -1,9 +1,15 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { Transform } from 'class-transformer';
 import { IsNotEmpty, IsString, MaxLength } from 'class-validator';
 export class CreateProfileDto {
   @ApiProperty({ example: 'Adeola' })
   @IsString()
   @IsNotEmpty()
   @MaxLength(50)
+  @Transform(({ value }) =>
+    typeof value === 'string'
+      ? value.replace(/\b\w/g, (c) => c.toUpperCase())
+      : value,
+  )
   name: string;
 }
