16e90245ad2329aa391255d330595819faaa6d12
Adeola Fadare <adeolafadare8@gmail.com>
2026-04-30 19:37:49 +0100

    fix(tests): fixed test errors

--- src/modules/auth/auth.controller.spec.ts
+++ src/modules/auth/auth.controller.spec.ts
@@ -1,5 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { PkceGuard } from './guard/pkce.guard';
 
 describe('AuthController', () => {
   let controller: AuthController;
@@ -7,6 +9,10 @@ describe('AuthController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AuthController],
+      providers: [
+        { provide: AuthService, useValue: {} },
+        { provide: PkceGuard, useValue: { canActivate: () => true } },
+      ],
     }).compile();
 
     controller = module.get<AuthController>(AuthController);
--- src/modules/auth/auth.service.spec.ts
+++ src/modules/auth/auth.service.spec.ts
@@ -1,12 +1,20 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
+import { UserService } from '../user/user.service';
 
 describe('AuthService', () => {
   let service: AuthService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      providers: [
+        AuthService,
+        { provide: JwtService, useValue: {} },
+        { provide: ConfigService, useValue: {} },
+        { provide: UserService, useValue: {} },
+      ],
     }).compile();
 
     service = module.get<AuthService>(AuthService);
